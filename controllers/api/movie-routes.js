78f9a732fd82bf9195ddf63d404f3e088862bbba
Rob Tatro <tatro009@gmail.com>
2023-12-19 20:30:21 -0600

    Added search.js, added movie search route, and updated main.handlebars with search form

--- controllers/api/movie-routes.js
+++ controllers/api/movie-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Movie, User } = require("../../models");
+const axios = require("axios");
 
 const express = require("express");
 const { authCheck } = require("../../utils/auth");
@@ -155,6 +156,29 @@ router.delete("/unwatch/:id", async (req, res) => {
       res.status(200).json(movieDeleted);
     })
     .catch((err) => res.json(err));
+});
+
+//New route for searching movies
+
+router.get("/search", async (req, res) => {
+  try {
+    const apiKey = 'process.env.API_KEY';
+    const query = req.query.query;
+
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
+      params: {
+        api_key: apiKey,
+        query: query,
+      },
+    });
+
+  const searchResults = response.data.results;
+
+  res.status(200).json(searchResults);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error"});
+  }
 });
 
 module.exports = router;
